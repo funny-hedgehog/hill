a20c656a8ff8035e6200d60041a5b3bb0d9d7f84
arifm <arifmho@yandex.ru>
2023-12-22 14:01:16 +0300

    'hill-5'

--- src/pages/OverLoadedPage.js
+++ src/pages/OverLoadedPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 const urls1 = [
-  'https://upload.wikimedia.org/wikipedia/commons/4/42/Plain_Landscape.jpg',
-];
+ 'https://www.esato.com/phonephotos/cam/samsung/sm_g900f/201406011711P0ER52.jpg',
+ 'https://nature.baikal.ru/phs/norm/84/84110.jpg',
+ 'https://prades.net/canada2004/493T1806.jpg',
 
-const urls2 = [
-  'https://wallbox.ru/wallpapers/main2/201717/nebo-dereva-ozero-gory-les-vodoem.jpg',
-]
+ 'https://i.pinimg.com/originals/55/49/0e/55490ee730c90b567d4d40bad287b6ca.jpg',
+ 'http://st.gde-fon.com/wallpapers_original/425001_vysokogornyj-otel_reka_pejzazh_priroda_1680x1050_www.Gde-Fon.com.jpg',
+
+
+ 'https://i.redd.it/v05doii2jwp31.jpg',
+ 'http://thereminder.ru/wp-content/uploads/2020/07/grand-canyon-crimea.jpg',
+ 'https://i.artfile.me/wallpaper/11-06-2013/4000x2667/priroda-reki-ozera-spirit-island-730985.jpg',
+ 'https://rondreizennoordamerika.nl/wp-content/uploads/2020/02/Valley-of-Fire-20190222-5N6A0728_1.jpg',
+ 'https://prades.net/canada2004/493T1914a.jpg',
+ 'https://photographylife.com/wp-content/uploads/2018/07/caPatchStart-1536x1103.jpg',
+ 'https://bugaga.ru/uploads/posts/2009-09/1251815154_01_09_2009_08628_philippe-sainte-laudy-6.jpg',
+ 'https://images.squarespace-cdn.com/content/v1/55c3df7ce4b0abe10a7ec1b9/1516168947908-K4NVIAE5KQDLFV10WAFQ/DJI_0140+-+Copy.jpg',
+ 'http://vsegda-pomnim.com/uploads/posts/2022-03/1647654770_66-vsegda-pomnim-com-p-gora-kostsyushko-foto-71.jpg',
+ // 'https://hdwallsbox.com/wallpapers/l/1920x1080/49/nature-panorama-1920x1080-48258.jpg',
+];
 
 const map = {
   'first': 'https://upload.wikimedia.org/wikipedia/commons/4/42/Plain_Landscape.jpg',
@@ -14,24 +27,24 @@ const map = {
 }
 
 function OverLoadedPage() {
-  const [url, setUrl] = React.useState('first')
-  const [photosSize, setPhotosSize] = React.useState(0)
+  // const [url, setUrl] = React.useState('first')
+  // const [photosSize, setPhotosSize] = React.useState(0)
   const [arraySize, setArraySize] = React.useState(0)
 
-  const imgRef = React.useRef();
+  // const imgRef = React.useRef();
 
-  React.useEffect(() => {
-    console.log('imgRef', imgRef.current.alt)
-    imgRef.current.onload = function() {
-      if (url === 'first') {
-        setUrl('second')
-        setPhotosSize(state => state = photosSize + 1.1)
-      } else {
-        setUrl('first')
-        setPhotosSize(state => state = photosSize + 1.1)
-      }
-    };
-  }, [url])
+  // React.useEffect(() => {
+  //   console.log('imgRef', imgRef.current.alt)
+  //   imgRef.current.onload = function() {
+  //     if (url === 'first') {
+  //       setUrl('second')
+  //       setPhotosSize(state => state = photosSize + 1.1)
+  //     } else {
+  //       setUrl('first')
+  //       setPhotosSize(state => state = photosSize + 1.1)
+  //     }
+  //   };
+  // }, [url])
 
 
   React.useEffect(() => {
@@ -47,43 +60,20 @@ function OverLoadedPage() {
     <div>
       <h1>OverLoadedPage</h1>
       <h2>Объём загруженного массива - {Math.round(arraySize / 1000)} MB</h2>
-      <h2>Объём загруженных фотографий  - {Math.trunc(photosSize)} MB</h2>
 
-      <img
-        ref={imgRef}
-        style={{opacity: 0}}
-        alt={url}
-        src={map[url]}
-        width={600}
-      />
-
-      {/*<img*/}
-      {/*  style={{opacity: 0}}*/}
-      {/*  onLoad={() => {*/}
-      {/*    const arr = url*/}
-      {/*    arr[index] = item === 'first' ? 'second' : 'first'*/}
-      {/*    setUrl([...arr])*/}
-      {/*    setPhotosSize(s => s += 1.1)*/}
-      {/*  }}*/}
-      {/* alt={`${index}`}*/}
-      {/* src={item === 'first' ? urls1[index] : urls2[index]}*/}
-      {/* width={600}*/}
-      {/*/>*/}
+      <ul>
+        {urls1.map(item => {
+          return (
+            <img
+              alt={'image'}
+              src={item}
+              width={600}
+            />
+          )
+        })}
+      </ul>
     </div>
   );
 }
 
 export default OverLoadedPage;
-
-/*
-onLoad={() => {
-          if (url === 'first') {
-            setUrl('second')
-            setPhotosSize(state => state = photosSize + 1.1)
-          } else {
-            setUrl('first')
-            setPhotosSize(state => state = photosSize + 1.1)
-          }
-
-        }}
- */
