134eb80cd3b54643718c72fac915a67dbc0b20a4
arifm <arifmho@yandex.ru>
2023-12-22 11:56:17 +0300

    'hill-3'

--- src/pages/OverLoadedPage.js
+++ src/pages/OverLoadedPage.js
@@ -8,15 +8,35 @@ const urls2 = [
   'https://wallbox.ru/wallpapers/main2/201717/nebo-dereva-ozero-gory-les-vodoem.jpg',
 ]
 
+const map = {
+  'first': 'https://upload.wikimedia.org/wikipedia/commons/4/42/Plain_Landscape.jpg',
+  'second': 'https://wallbox.ru/wallpapers/main2/201717/nebo-dereva-ozero-gory-les-vodoem.jpg',
+}
+
 function OverLoadedPage() {
-  const [url, setUrl] = React.useState(['first'])
+  const [url, setUrl] = React.useState('first')
   const [photosSize, setPhotosSize] = React.useState(0)
   const [arraySize, setArraySize] = React.useState(0)
+
+  const imgRef = React.useRef();
+
+  React.useEffect(() => {
+    console.log('imgRef', imgRef.current.alt)
+    imgRef.current.onload = function() {
+      if (url === 'first') {
+        setUrl('second')
+        setPhotosSize(state => state = photosSize + 1.1)
+      } else {
+        setUrl('first')
+        setPhotosSize(state => state = photosSize + 1.1)
+      }
+    };
+  }, [url])
+
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/photos')
       .then(response => {
-        console.log(response)
         return response.json()
       })
       .then(json => setArraySize(s => s += 135))
@@ -26,41 +46,44 @@ function OverLoadedPage() {
   return (
     <div>
       <h1>OverLoadedPage</h1>
-      {/*<img onLoad={() => {*/}
-      {/*  if (url === 'first') {*/}
-      {/*    console.log('Страница', url);*/}
-      {/*    setUrl('second')*/}
-      {/*  } else {*/}
-      {/*    console.log('Страница загружена', url);*/}
-      {/*    setUrl('first')*/}
-      {/*  }*/}
-      {/*}} alt={url} src={map[url]} width={600} />*/}
-
       <h2>Объём загруженного массива - {Math.round(arraySize / 1000)} MB</h2>
-      <h2>Объём загруженных фотографий  - {Math.round(photosSize)} MB</h2>
+      <h2>Объём загруженных фотографий  - {Math.trunc(photosSize)} MB</h2>
 
-      <ul style={{listStyle: 'none'}}>
-        {url.map((item, index) => {
-          return (
-            <li key={index}>
-              <img
-                style={{opacity: 0}}
-                onLoad={() => {
-                  const arr = url
-                  arr[index] = item === 'first' ? 'second' : 'first'
-                  setUrl([...arr])
-                  setPhotosSize(s => s += 1.1)
-                }}
-               alt={`${index}`}
-               src={item === 'first' ? urls1[index] : urls2[index]}
-               width={600}
-              />
-            </li>
-          )
-        })}
-      </ul>
+      <img
+        ref={imgRef}
+        style={{opacity: 0.1}}
+        alt={url}
+        src={map[url]}
+        width={600}
+      />
+
+      {/*<img*/}
+      {/*  style={{opacity: 0}}*/}
+      {/*  onLoad={() => {*/}
+      {/*    const arr = url*/}
+      {/*    arr[index] = item === 'first' ? 'second' : 'first'*/}
+      {/*    setUrl([...arr])*/}
+      {/*    setPhotosSize(s => s += 1.1)*/}
+      {/*  }}*/}
+      {/* alt={`${index}`}*/}
+      {/* src={item === 'first' ? urls1[index] : urls2[index]}*/}
+      {/* width={600}*/}
+      {/*/>*/}
     </div>
   );
 }
 
 export default OverLoadedPage;
+
+/*
+onLoad={() => {
+          if (url === 'first') {
+            setUrl('second')
+            setPhotosSize(state => state = photosSize + 1.1)
+          } else {
+            setUrl('first')
+            setPhotosSize(state => state = photosSize + 1.1)
+          }
+
+        }}
+ */
