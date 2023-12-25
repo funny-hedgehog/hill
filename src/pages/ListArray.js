95b7fb8307a5afaed2462a7346bfca4289dd3f5b
arifm <arifmho@yandex.ru>
2023-12-25 10:56:24 +0300

    array-3

--- src/pages/ListArray.js
+++ src/pages/ListArray.js
@@ -1,32 +1,48 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import data4 from './data4.json';
 
 const arr = []
 
 export default React.memo(
   function ListArray() {
-    // React.useEffect(() => {
-    //   for (let i = 0; i < 100; i++) {
-    //     const obj = {
-    //       "albumId": 100,
-    //       "id": i,
-    //       "title": "qui quo cumque distinctio aut voluptas",
-    //       "url": "https://via.placeholder.com/600/315aa6",
-    //       "thumbnailUrl": "https://via.placeholder.com/150/315aa6"
-    //     }
-    //
-    //     arr.push(obj)
-    //   }
-    // }, [arr])
+    const [product, setProducts] = React.useState([])
+    const timer = React.useRef()
+
+    const handleAddItem = () => {
+      const newItem = {
+        "albumId": 100,
+        "id": uuidv4(),
+        "title": "qui quo cumque distinctio aut voluptas",
+        "url": "https://via.placeholder.com/600/315aa6",
+        "thumbnailUrl": "https://via.placeholder.com/150/315aa6"
+      }
+      console.log('GGG')
+      setProducts((prev) => [...prev, newItem])
+    }
+
+    React.useEffect(() => {
+      timer.current = setInterval(handleAddItem, 50)
+
+      return () => {
+        clearInterval(timer.current)
+      }
+    }, [])
 
     return (
-      <ul>
-        {data4.map(({id}) => {
-          return (
-            <li key={id}>{id}</li>
-          )
-        })}
-      </ul>
+      <>
+        <ul>
+          {product.map(({id}) => {
+            return (
+              <li key={id}>{id}</li>
+            )
+          })}
+        </ul>
+        {/*<button onClick={handleAddItem}>*/}
+        {/*  CLICK*/}
+        {/*</button>*/}
+      </>
+
     );
   },
   (prevProps, nextPropx) => {
